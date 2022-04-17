dc0b6461548d532f52170f7304d061dec9af336a
grayweling <jy6228@yahoo.com>
2022-04-17 12:01:46 -0500

    added password verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,6 +55,29 @@ router.post('/', async (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    //expects email and password
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }) .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address' });
+            return;
+        }
+
+        //verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    })
+})
+
 //PUT /api/users/1
 router.put('/:id', async (req, res) => {
     //expects {username: 'someusername', email: 'someemail', password: 'somepassword'}
